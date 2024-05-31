06193e1ee37f7d6c2307edfdf2a3b449523ad2d2
PSHEMA <p.shema@alustudent.com>
2024-05-31 17:19:42 +0200

    Home

---
+++ group_project/lib/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Welcome message
+          Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Welcome to RwandaExplore App! Please login or register.',
+                    style: TextStyle(fontSize: 24),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+          // Login and register buttons
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Navigate to login screen
+                  Navigator.pushNamed(context, '/login');
+                },
+                child: Text('Login'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Navigate to registration screen
+                  Navigator.pushNamed(context, '/register');
+                },
+                child: Text('Register'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
